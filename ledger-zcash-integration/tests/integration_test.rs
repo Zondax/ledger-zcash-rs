734ab02ddcb3f02389f01e6d021cd87387d9fc27
Leon Groot Bruinderink <lgrootbr@gmail.com>
2020-11-13 14:33:13 +0100

    integration test sapling transaction

--- ledger-zcash-integration/tests/integration_test.rs
+++ ledger-zcash-integration/tests/integration_test.rs
@@ -27,7 +27,12 @@ extern crate ledger_zcash;
 #[cfg(test)]
 mod integration_tests {
     use env_logger::Env;
-    use ledger_zcash::{APDUTransport, ZcashApp, PK_LEN_SAPLING, PK_LEN_SECP261K1};
+    use ledger_zcash::{APDUTransport, ZcashApp, PK_LEN_SAPLING, PK_LEN_SECP261K1, *};
+    use zcash_primitives::keys::OutgoingViewingKey;
+    use zcash_primitives::merkle_tree::IncrementalWitness;
+    use zcash_primitives::primitives::PaymentAddress;
+    use zcash_primitives::primitives::Rseed;
+    use zcash_primitives::transaction::components::Amount;
     use zx_bip44::BIP44Path;
 
     fn init_logging() {
@@ -188,5 +193,95 @@ mod integration_tests {
             response.err().unwrap(),
             ledger_zcash::LedgerAppError::InvalidEmptyMessage
         ));
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn do_full_transaction_shieldedonly() {
+        init_logging();
+
+        let transport = APDUTransport {
+            transport_wrapper: Box::new(ledger::TransportNativeHID::new().unwrap()),
+        };
+        let app = ZcashApp::new(transport);
+
+        let spend1 = LedgerDataShieldedSpend {
+            path: 1000,
+            address: PaymentAddress::from_bytes(&[
+                198, 158, 151, 156, 103, 99, 193, 176, 146, 56, 220, 107, 213, 220, 191, 53, 54,
+                13, 249, 93, 202, 223, 140, 15, 162, 93, 203, 237, 170, 246, 5, 117, 56, 184, 18,
+                208, 102, 86, 114, 110, 162, 118, 103,
+            ])
+            .unwrap(),
+            value: Amount::from_u64(75_000).unwrap(),
+            witness: IncrementalWitness::read(
+                &hex::decode(
+                    "01305aef35a6fa9dd43af22d2557f99268fbab70a53e963fa67fc762391510406000000000",
+                )
+                .unwrap()[..],
+            )
+            .unwrap(),
+            rseed: Rseed::AfterZip212([0u8; 32]),
+        };
+
+        let spend2 = LedgerDataShieldedSpend {
+            path: 1000,
+            address: PaymentAddress::from_bytes(&[
+                198, 158, 151, 156, 103, 99, 193, 176, 146, 56, 220, 107, 213, 220, 191, 53, 54,
+                13, 249, 93, 202, 223, 140, 15, 162, 93, 203, 237, 170, 246, 5, 117, 56, 184, 18,
+                208, 102, 86, 114, 110, 162, 118, 103,
+            ])
+            .unwrap(),
+            value: Amount::from_u64(25_000).unwrap(),
+            witness: IncrementalWitness::read(
+                &hex::decode(
+                    "01305aef35a6fa9dd43af22d2557f99268fbab70a53e963fa67fc762391510406000000000",
+                )
+                .unwrap()[..],
+            )
+            .unwrap(),
+            rseed: Rseed::AfterZip212([0xFF; 32]),
+        };
+
+        let output1 = LedgerDataShieldedOutput {
+            value: Amount::from_u64(80_000).unwrap(),
+            address: PaymentAddress::from_bytes(&[
+                21, 234, 231, 0, 224, 30, 36, 226, 19, 125, 85, 77, 103, 187, 13, 166, 78, 238, 11,
+                241, 194, 195, 146, 197, 241, 23, 58, 151, 155, 174, 184, 153, 102, 56, 8, 205, 34,
+                237, 141, 242, 117, 102, 204,
+            ])
+            .unwrap(),
+            ovk: None,
+            memo: None,
+        };
+
+        let txfee = Amount::from_u64(1000).unwrap();
+        let change_amount = spend1.value + spend2.value - output1.value - txfee;
+
+        let output2 = LedgerDataShieldedOutput {
+            value: change_amount,
+            address: PaymentAddress::from_bytes(&[
+                198, 158, 151, 156, 103, 99, 193, 176, 146, 56, 220, 107, 213, 220, 191, 53, 54,
+                13, 249, 93, 202, 223, 140, 15, 162, 93, 203, 237, 170, 246, 5, 117, 56, 184, 18,
+                208, 102, 86, 114, 110, 162, 118, 103,
+            ])
+            .unwrap(),
+            ovk: Some(OutgoingViewingKey([
+                111, 192, 30, 170, 102, 94, 3, 165, 60, 30, 3, 62, 208, 215, 123, 103, 12, 240,
+                117, 237, 228, 173, 167, 105, 153, 122, 46, 210, 236, 34, 95, 202,
+            ])),
+            memo: None,
+        };
+
+        let input = LedgerDataInput {
+            txfee: 1000,
+            vec_tin: vec![],
+            vec_tout: vec![],
+            vec_sspend: vec![spend1, spend2],
+            vec_soutput: vec![output1, output2],
+        };
+
+        let response = app.do_transaction(&input).await;
+        assert!(response.is_ok());
     }
 }
